e61e8e70900ecb732f50488aec8e5f69c3bd5859
Vrunda Joshi <vrunda1002@gmail.com>
2024-07-29 16:20:59 +0530

    Using event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.status(200).send(comments);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const {content} = req.body;
   const commentId = randomBytes(4).toString('hex');
 
@@ -26,7 +27,17 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {id: commentId, content, postId: req.params.id},
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received an event', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => console.log('Listening on port 4001'));
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
-  const {event} = req.body;
+  const event = req.body;
+  console.log('Received an event', req.body);
 
   axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
