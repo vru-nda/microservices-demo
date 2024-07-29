ba9f1f198f1d9dc95987578ea4b6618d212b5440
Vrunda Joshi <vrunda1002@gmail.com>
2024-07-29 18:29:43 +0530

    Dealing with missing events by event syncing

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
@@ -26,5 +30,7 @@ app.post('/events', (req, res) => {
 
   res.send({status: 'OK'});
 });
+
+app.get('/events', (req, res) => res.send(events));
 
 app.listen(4005, () => console.log('Listening on port 4005'));
